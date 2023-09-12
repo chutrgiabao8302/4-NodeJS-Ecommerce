d1da180689dcf9cb44b81a3dc694cfae36c3c50a
chutrgiabao8302 <520h0011@student.tdtu.edu.vn>
2023-09-12 16:55:26 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Cart open close
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+// Open cart
+cartIcon.onclick = () => {
+    cart.classList.add('active');
+}
+// Close cart 
+closeCart.onclick = () => {
+    cart.classList.remove('active');
+}
